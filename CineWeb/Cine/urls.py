b693993e973cdef7a1aa4c69206e051224cfdb4d
josema-montano <montanojm336@gmail.com>
2024-08-22 13:15:48 -0400

    Vista de detalles Pelicula

--- CineWeb/Cine/urls.py
+++ CineWeb/Cine/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import detalles_view
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('detalles/', detalles_view, name='detalles')
 ]
--- CineWeb/Cine/views.py
+++ CineWeb/Cine/views.py
@@ -5,3 +5,5 @@ def inicio(request):
     # Ordenar por fecha de creación en orden descendente y obtener los últimos 10 registros
     peliculas = Pelicula.objects.all().order_by('Lanzamiento')[:10]
     return render(request, r'cine/inicio.html', {'peliculas': peliculas})
+def detalles_view(request):
+    return render(request, 'DetallesPeli.html')
